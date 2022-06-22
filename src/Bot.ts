d0301a02a36cf89104597462572b919125d783e1
sparib <sparibtheonly@gmail.com>
2022-06-21 22:47:13 -0400

    Bot can now login (theoretically)

--- src/Bot.ts
+++ src/Bot.ts
@@ -1,2 +1,14 @@
 import { get_tweet } from './twitter';
+import { Client } from "discord.js";
+import dotenv from 'dotenv';
 
+dotenv.config();
+
+console.log("Bot is starting...");
+
+const client = new Client({
+    intents: []
+});
+client.login(process.env.DISCORD_TOKEN);
+
+console.log(client);
