6f7b31b157efc595ad875e6498d1b3e7aaf8a27f
sparib <sparibtheonly@gmail.com>
2022-06-25 23:09:25 -0400

    Unused imports

--- src/twitter.ts
+++ src/twitter.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
+import axios, { AxiosResponse } from 'axios';
 import { randomInt } from 'crypto';
 import { config } from 'dotenv';
 
